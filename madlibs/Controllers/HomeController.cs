a597c3bc95bdcdfd518a964e54665f9fab5c7a62
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-10 15:19:26 -0700

    add two user madlibs with return to home button
    Co-authored-by: Cristina Plesa <cristinaplesa8@gmail.com>

--- madlibs/Controllers/HomeController.cs
+++ madlibs/Controllers/HomeController.cs
@@ -5,19 +5,36 @@ namespace MadLibs.Controllers
 {
   public class HomeController : Controller
   {
-
     [Route("/")]
-    public ActionResult Form() { return View(); }
+    public ActionResult Home() { return View(); }
 
-    [Route("/Story")]
-    public ActionResult Story(string adjective, string noun, string noun2, string pastTenseVerb)
+    [Route("/FairyForm")]
+    public ActionResult FairyForm() { return View(); }
+
+    [Route("/FairyTale")]
+    public ActionResult FairyTale(string adjective, string noun, string noun2, string pastTenseVerb)
     {
-      MadLibsGame firstMabLibs = new MadLibsGame();
-      firstMabLibs.Adjective = adjective;
-      firstMabLibs.Noun = noun;
-      firstMabLibs.Noun2 = noun2;
-      firstMabLibs.PastTenseVerb = pastTenseVerb;
-      return View(firstMabLibs);
+      MadLibsGame FairyMadLibs = new MadLibsGame();
+      FairyMadLibs.Adjective = adjective;
+      FairyMadLibs.Noun = noun;
+      FairyMadLibs.Noun2 = noun2;
+      FairyMadLibs.PastTenseVerb = pastTenseVerb;
+      return View(FairyMadLibs);
+    }
+
+    [Route("/SciFiForm")]
+    public ActionResult SciFiForm() { return View(); }
+
+    [Route("/SciFiTale")]
+    public ActionResult SciFiTale(string adjective, string noun, string noun2, string noun3, string pastTenseVerb)
+    {
+      MadLibsGame SciFiMadLibs = new MadLibsGame();
+      SciFiMadLibs.Adjective = adjective;
+      SciFiMadLibs.Noun = noun;
+      SciFiMadLibs.Noun2 = noun2;
+      SciFiMadLibs.Noun2 = noun3;
+      SciFiMadLibs.PastTenseVerb = pastTenseVerb;
+      return View(SciFiMadLibs);
     }
   }
 }
--- madlibs/Models/madlibs.cs
+++ madlibs/Models/madlibs.cs
@@ -5,6 +5,8 @@ namespace MadLibs.Models
     public string Adjective { get; set; }
     public string Noun { get; set; }
     public string Noun2 { get; set; }
-    public string PastTenseVerb { get; set; }
+    public string Noun3 { get; set; }
+    public string PastTenseVerb
+    { get; set; }
   }
 }
